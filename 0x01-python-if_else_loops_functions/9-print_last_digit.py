47be4e47817a88d414b07efe7013ac58dc5662d2
bahailu-abera <aberabahailu4@gmail.com>
2022-06-01 11:20:44 -0700

    return value

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -5,4 +5,6 @@ def print_last_digit(number):
         last_digit = (-1 * number) % 10
     else:
         last_digit = number % 10
-    print(last_digit)
+    print(last_digit, end="")
+
+    return last_digit
