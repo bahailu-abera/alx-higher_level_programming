0ab52969d2c557e70ef6f1e55cc30a0598024442
bahailu-abera <aberabahailu4@gmail.com>
2022-06-02 14:49:31 -0700

    suffix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
     argv = sys.argv
     length = len(argv)
 
-    print("{} arguments{}".format(length - 1, '.' if length == 1 else ':'))
+    print("{} argument{}{}".format(length - 1, '' if length == 2 else 's',
+                                   '.' if length == 1 else ':'))
 
     for argnum in range(1, length):
         print("{}: {}".format(argnum, argv[argnum]))
