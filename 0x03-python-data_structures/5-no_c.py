bb0130264f02e6ca4d188b2285fd42fee28134da
bahailu-abera <aberabahailu4@gmail.com>
2022-06-04 13:11:49 -0700

    no c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new_string = ""
+    for let in my_string:
+        if let in "cC":
+            continue
+        new_string += let
+    return new_string
