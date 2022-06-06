6bc14baf5df9ce1bfeca24020d60dd0d8d9909a7
bahailu-abera <aberabahailu4@gmail.com>
2022-06-06 00:53:25 -0700

    divisible by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    div_by_2 = []
+
+    if my_list:
+        for num in my_list:
+            if num % 2 == 0:
+                div_by_2.append(True)
+            else:
+                div_by_2.append(False)
+        return div_by_2
