64b779438de9a29e295e3b72fbb55c48dc6aef11
bahailu abera <aberabahailu4@gmail.com>
2022-06-06 11:09:03 +0300

    Update 11-delete_at.py
    handle None

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
+    if not my_list:
+        pass
+    elif idx < 0 or idx >= len(my_list):
         return my_list
     return my_list[:idx] + my_list[idx + 1:]
