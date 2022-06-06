dc405dabc3c12ba343e8251f7ef40d3f436f8187
bahailu-abera <aberabahailu4@gmail.com>
2022-06-06 13:54:27 -0700

    handle all case

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,6 +8,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
         return (0, 0)
     elif len_a == 0 and len_b == 1:
         return (tuple_b[0], 0)
+    elif len_a == 0 and len_b >= 2:
+        return (tuple_b[0], tuple_b[1])
+    elif len_b == 0 and len_a >= 2:
+        return (tuple_a[0],tuple_ a[1])
     elif len_a == 1 and len_b == 0:
         return (tuple_a[0], 0)
     elif len_a == 1 and len_b == 1:
