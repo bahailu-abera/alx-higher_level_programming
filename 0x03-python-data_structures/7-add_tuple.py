32588cb0202cb5be00fa0d8f0098af21efb99b37
bahailu-abera <aberabahailu4@gmail.com>
2022-06-06 13:49:13 -0700

    rename function name

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 
-def add(a, b):
-    len_a = len(a)
-    len_b = len(b)
+def add_tuple(tuple_a=(), tuple_b=()):
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
 
     if len_a == 0 and len_b == 0:
         return (0, 0)
     elif len_a == 0 and len_b == 1:
-        return (b[0], 0)
+        return (tuple_b[0], 0)
     elif len_a == 1 and len_b == 0:
-        return (a[0], 0)
+        return (tuple_a[0], 0)
     elif len_a == 1 and len_b == 1:
-        return (a[0] + b[0], 0)
+        return (tuple_a[0] + tuple_b[0], 0)
     elif len_a == 1 and len_b == 2:
-        return (a[0] + b[0], b[1])
+        return (tuple_a[0] + tuple_b[0], tuple_b[1])
     elif len_a == 2 and len_b == 1:
-        return (a[0] + b[0], a[1])
+        return (tuple_a[0] + tuple_b[0], tuple_a[1])
     else:
-        return (a[0] + b[0], a[1] + b[1])
+        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
