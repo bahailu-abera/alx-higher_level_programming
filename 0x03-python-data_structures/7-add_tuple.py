587cb4272b855547ac6e9940b17a22fcab41324b
bahailu abera <aberabahailu4@gmail.com>
2022-06-07 00:03:32 +0300

    Update 7-add_tuple.py
    efficient approach

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,24 +1,9 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    len_a = len(tuple_a)
-    len_b = len(tuple_b)
-
-    if len_a == 0 and len_b == 0:
-        return (0, 0)
-    elif len_a == 0 and len_b == 1:
-        return (tuple_b[0], 0)
-    elif len_a == 0 and len_b >= 2:
-        return (tuple_b[0], tuple_b[1])
-    elif len_b == 0 and len_a >= 2:
-        return (tuple_a[0],tuple_ a[1])
-    elif len_a == 1 and len_b == 0:
-        return (tuple_a[0], 0)
-    elif len_a == 1 and len_b == 1:
-        return (tuple_a[0] + tuple_b[0], 0)
-    elif len_a == 1 and len_b == 2:
-        return (tuple_a[0] + tuple_b[0], tuple_b[1])
-    elif len_a == 2 and len_b == 1:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1])
-    else:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    tuple_a = tuple_a + (0, 0)
+    tuple_b = tuple_b + (0, 0)
+    
+    result = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    
+    return result
