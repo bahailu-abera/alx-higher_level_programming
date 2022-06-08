ca8288393b7901aa65cdb3e83609dbab6087e2b4
bahailu-abera <aberabahailu4@gmail.com>
2022-06-08 14:35:16 -0700

    use append

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,7 +5,7 @@ def search_replace(my_list, search, replace):
         new_list = []
         for idx in range(len(my_list)):
             if my_list[idx] == search:
-                new_ist[idx] = replace
+                new_list.append(replace)
             else:
-                new_list[idx] = my_list[idx]
+                new_list.append(my_list[idx])
         return new_list
