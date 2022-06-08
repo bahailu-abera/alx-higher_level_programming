403251caacb0c27f031b41da488ffb322d0d30fa
bahailu-abera <aberabahailu4@gmail.com>
2022-06-08 14:27:13 -0700

    don't modify original

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,10 @@
 
 def search_replace(my_list, search, replace):
     if my_list:
+        new_list = []
         for idx in range(len(my_list)):
             if my_list[idx] == search:
-                my_list[idx] = replace
-    return my_list
+                new_ist[idx] = replace
+            else:
+                new_list[idx] = my_list[idx]
+    return new_list
