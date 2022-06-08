967d566e51912411e6fdfe9f5c431d3d59341c04
bahailu-abera <aberabahailu4@gmail.com>
2022-06-08 16:00:24 -0700

    handle empty set

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,7 +2,7 @@
 
 def only_diff_elements(set_1, set_2):
     diff = set()
-    if set_1 and set_2:
+    if isinstance(set_1, set) and isinstance(set_2, set):
         diff = (set_1 - set_2) | (set_2 - set_1)
 
     return diff
