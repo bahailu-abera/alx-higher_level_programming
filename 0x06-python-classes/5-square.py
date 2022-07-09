2f0a70469e959916a5c23601d22de2edcdf40fa0
bahailu <aberabahailu4@gmail.com>
2022-07-09 23:25:28 +0300

    type is int

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -32,7 +32,7 @@ class Square:
 
     def my_print(self):
         """ Prints in stdout the square with the character #. """
-        if int(self.__size) == 0:
+        if self.__size == 0:
             print()
         else:
             for i in range(self.__size):
