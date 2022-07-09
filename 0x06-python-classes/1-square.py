b6c0b6cc728dc5f30d50b8e50c5a5ebfab1d1f26
bahailu <aberabahailu4@gmail.com>
2022-07-09 15:29:43 +0300

    attribute added

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+""" Object Oriented Programming Fundamental module """
+
+#!/usr/bin/python3
+
+
+class Square:
+    """ The object representation of a square with size attribute """
+    def __init__(self, size):
+        self.__size = size
