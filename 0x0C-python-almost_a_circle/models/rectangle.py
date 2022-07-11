5da6d2b811813f43b8107d5edc5b37225564d760
bahailu <aberabahailu4@gmail.com>
2022-07-11 14:33:22 +0300

    display method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -90,3 +90,8 @@ class Rectangle(Base):
     def area(self):
         """ Returns the area of the rectangle """
         return self.__width * self.__height
+
+    def display(self):
+        """ Prints in stdout the Rectangle instance with the character '#' """
+        for _ in range(self.__height):
+            print("#" * self.__width)
