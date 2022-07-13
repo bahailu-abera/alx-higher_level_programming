3eaf29bb92345fac3c8ce58bda085ca9a81d01bd
bahailu <aberabahailu4@gmail.com>
2022-07-13 21:08:20 +0300

    cpython

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,3 +1,4 @@
+#define PY_SSIZE_T_CLEAN
 #include <Python.h>
 #include <stdio.h>
 
@@ -10,38 +11,71 @@
  */
 void print_python_bytes(PyObject *p)
 {
-	long int length = 0, count, limit;
 	char *string;
+	long int size, i, limit;
+
+	setbuf(stdout, NULL);
 
 	printf("[.] bytes object info\n");
-
 	if (!PyBytes_Check(p))
 	{
-		printf("  [ERROR] Invalid Bytes Object\n");
+		printf(" [ERROR] Invalid Bytes Object\n");
+		setbuf(stdout, NULL);
 		return;
 	}
 
-	length = PyBytes_Size(p);
+	size = ((PyVarObject *)p)->ob_size;
 	string = ((PyBytesObject *)p)->ob_sval;
-	printf("  size: %ld\n", length);
-	printf("  trying string: %s\n", string);
 
-	if (length >= 10)
+	printf(" size: %ld\n", size);
+	printf(" trying string: %s\n", string);
+
+	if (size >= 10)
 		limit = 10;
 	else
-		limit = length + 1;
+		limit = size + 1;
 
-	printf("  first %ld bytes:", limit);
-	for (count = 0; count < limit; count++)
-	{
-		if (string[count] >= 0)
-			printf(" %02x", string[count]);
+	printf(" first %ld bytes:", limit);
+
+	for (i = 0; i < limit; i++)
+		if (string[i] >= 0)
+			printf(" %02x", string[i]);
 		else
-			printf(" %02x", 256 + string[count]);
+			printf(" %02x", 256 + string[i]);
+	printf("\n");
+	setbuf(stdout, NULL);
+}
+
+/**
+ * print_python_float - print some basic info about python float
+ *
+ * @p: PyObjecy
+ *
+ * Return: void
+ */
+void print_python_float(PyObject *p)
+{
+	double val;
+	char *nf;
+
+	setbuf(stdout, NULL);
+	printf("[.] float object info\n");
+
+	if (!PyFloat_Check(p))
+	{
+		printf(" [ERROR] Invalid Float Object\n");
+		setbuf(stdout, NULL);
+		return;
 	}
 
-	printf("\n");
+	val = ((PyFloatObject *)p)->ob_fval;
+	nf = PyOS_double_to_string(val, 'r', 0, Py_DTSF_ADD_DOT_0,
+				   Py_DTST_FINITE);
+
+	printf(" value: %s\n", nf);
+	setbuf(stdout, NULL);
 }
+
 
 /**
  * print_python_list - printf some basic info about Python lists
@@ -51,23 +85,35 @@ void print_python_bytes(PyObject *p)
  */
 void print_python_list(PyObject *p)
 {
-	long int length, count;
+	long int size, count;
 	PyListObject *list;
 	PyObject *obj;
 
-	length = PyList_Size(p);
+	setbuf(stdout, NULL);
 	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", length);
 
+	if (!PyList_Check(p))
+	{
+		printf(" [ERROR] Invalid List Object\n");
+		setbuf(stdout, NULL);
+		return;
+	}
+
+	size = ((PyVarObject *)p)->ob_size;
 	list = (PyListObject *)p;
+
+	printf("[*] Size of the Python list = %ld\n", size);
 	printf("[*] Allocated = %ld\n", list->allocated);
 
-	for (count = 0; count < length; count++)
+	for (count = 0; count < size; count++)
 	{
-		obj = ((PyListObject *)p)->ob_item[count];
+		obj = list->ob_item[count];
 		printf("Element %ld: %s\n", count, ((obj)->ob_type)->tp_name);
 
 		if (PyBytes_Check(obj))
 			print_python_bytes(obj);
+		if (PyFloat_Check(obj))
+			print_python_float(obj);
 	}
+	setbuf(stdout, NULL);
 }
