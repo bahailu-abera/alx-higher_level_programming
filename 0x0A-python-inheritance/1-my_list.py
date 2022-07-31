2daf4c77f99556179f1c0d5067c194ce652d3ddd
bahailu <aberabahailu4@gmail.com>
2022-08-01 00:27:54 +0300

    empty list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,8 +6,10 @@ class MyList(list):
     """ Object representation of MyList """
     def print_sorted(self):
         """ Sorts a list in ascending order """
+        new = []
         if not self or len(self) <= 0:
             pass
         else:
             new = self[:]
-            print(sorted(new))
+
+        print(sorted(new))
