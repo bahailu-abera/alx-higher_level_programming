74c937353a460c48e96cef4f5f7b5a51d4276958
bahailu <aberabahailu4@gmail.com>
2022-08-01 22:21:25 +0300

    ne added

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,3 +6,6 @@ class MyInt(int):
     """ Custom int class """
     def __eq__(self, __x):
         return not super().__eq__(__x)
+
+    def __ne__(self, __x):
+        return not super().__ne__(__x)
