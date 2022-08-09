d27a29246f1d6c5cd99db130cbb00120ad070767
bahailu <aberabahailu4@gmail.com>
2022-08-09 22:49:47 +0300

    first work

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- List all records by order
+-- Query to list a record by order
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- Updates score
+-- Query to update the score
+UPDATE second_table set score=10 where name="Bob";
