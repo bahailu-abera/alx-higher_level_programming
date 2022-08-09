37a80cd51a75758b16a419d7a65566a372f62632
bahailu <aberabahailu4@gmail.com>
2022-08-10 01:58:22 +0300

    upper case

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- Number of records with the same score
 -- Query to retrieve number of records with the same score
-SELECT score, COUNT(score) as number FROM second_table GROUP BY score;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score;
