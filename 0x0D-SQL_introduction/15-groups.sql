0a3ab6aaeaf2b69835ebaf1f44486bd55f5f9a0a
bahailu <aberabahailu4@gmail.com>
2022-08-10 02:03:11 +0300

    sort

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,6 @@
 -- Number of records with the same score
 -- Query to retrieve number of records with the same score
-SELECT score, COUNT(score) AS 'number' FROM second_table GROUP BY score;
+SELECT score, COUNT(score) AS 'number'
+FROM second_table
+GROUP BY score
+ORDER BY score DESC;
