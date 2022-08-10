9ffce5466e4121a1edc8ca9fdaf4b01e8bd62800
bahailu <aberabahailu4@gmail.com>
2022-08-10 15:24:37 +0300

    encoding

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,7 @@
+-- Change mysql database to utf8 encoding
+-- Query to change database to utf8 encoding
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name VARCHAR(256)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
