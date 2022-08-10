4e81f7ba8be05aae8e85b3ba163b589792521aa4
bahailu <aberabahailu4@gmail.com>
2022-08-10 15:37:19 +0300

    convert

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,4 +2,5 @@
 -- Query to change database to utf8 encoding
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
-ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
