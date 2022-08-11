4477ea1fa728c5a7a3ea0f0c0feb7a7384ab2a1b
bahailu <aberabahailu4@gmail.com>
2022-08-11 19:31:05 +0300

    state

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- Creates database and table
+-- Query to create database and table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256));
