cb94fe9ef7d13d578d1053b10bdafceef5ec7199
bahailu <aberabahailu4@gmail.com>
2022-08-11 18:05:26 +0300

    updated

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- Create user
 -- Query to create user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' DENTIFIED BY 'user_0d_1_pwd';
