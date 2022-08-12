3f10fad87d7d6be7d71df6bd43ddb5f036f73ad6
bahailu <aberabahailu4@gmail.com>
2022-08-12 18:14:46 +0300

    names as genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Count list of all genres with shows
 -- Query to count and display all shows of genres
-SELECT tg.name, COUNT(tg.name) AS number_of_shows
+SELECT tg.name as genre, COUNT(tg.name) AS number_of_shows
 FROM tv_genres tg INNER JOIN tv_show_genres tsg
 ON tg.id = tsg.genre_id
 GROUP BY tg.name
