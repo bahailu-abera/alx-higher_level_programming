4f82433ceb292ea32bb10e72c13804ad0a101642
bahailu <aberabahailu4@gmail.com>
2022-09-08 15:04:42 +0300

    semistandard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const argv = process.argv;
 
-arg = Number(argv[2]);
+const arg = Number(argv[2]);
 if (Number.isSafeInteger(arg)) {
-    console.log(`My number: ${arg}`);
+  console.log(`My number: ${arg}`);
 } else {
-    console.log("Not a Number");
+  console.log('Not a Number');
 }
