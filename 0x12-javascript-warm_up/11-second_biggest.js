8fd87686f22b0aa11ed8341f2b813307901545f5
bahailu <aberabahailu4@gmail.com>
2022-09-09 15:26:47 +0300

    type cast

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const argv = process.argv;
 
-let secondLargest = 0; let largest = argv[2];
+let secondLargest = 0; let largest = Number(argv[2]);
 for (let i = 3; i < argv.length; i++) {
-  if (argv[i] > largest) {
+  if (Number(argv[i]) > largest) {
     secondLargest = largest;
-    largest = argv[i];
-  } else if (argv[i] > secondLargest) {
-    secondLargest = argv[i];
+    largest = Number(argv[i]);
+  } else if (Number(argv[i]) > secondLargest) {
+    secondLargest = Number(argv[i]);
   }
 }
 console.log(secondLargest);
