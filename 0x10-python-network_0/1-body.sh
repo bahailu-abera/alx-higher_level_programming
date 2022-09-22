d19ac8a7426782bb330712a3da4aaa07bd56efdc
bahailu <aberabahailu4@gmail.com>
2022-09-22 23:37:03 +0300

    use optional

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # a script to displays the body of the response
-status="$(curl -sI "$1"  | head -n 1 | cut -d " " -f 2)"
-
-if [ "$status" = 200 ]
-then
-    curl "$1"
-fi
+curl -Ls "$1"
