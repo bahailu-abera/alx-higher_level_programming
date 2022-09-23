dc9d26d376cd9b571e745fe5135f4cdc4217014b
bahailu <aberabahailu4@gmail.com>
2022-09-23 11:45:15 +0300

    use post

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a script to send post request
-curl  -d 'email=test@gmail.com&subject=I will always be here for PLD' "$1"
+curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" -X POST  "$1"
