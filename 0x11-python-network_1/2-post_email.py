faffb87400ded26ded6fd55b7d6a1a86bfb52273
bahailu <aberabahailu4@gmail.com>
2022-09-24 19:39:23 +0300

    post email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module for posting email to a server
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+def post_email(url, email):
+    """
+    post an email to the specified url
+    """
+    data = {}
+    data['email'] = email
+    params = urllib.parse.urlencode(data)
+    full_url = url + '?' + params
+    with urllib.request.urlopen(url) as res:
+        print(res.read().decode("utf-8"))
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    post_email(url, email)
