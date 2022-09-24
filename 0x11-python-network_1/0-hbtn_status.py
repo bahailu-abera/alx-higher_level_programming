be69536243fd509d29500da4fbdba8e0d2c1a20c
bahailu <aberabahailu4@gmail.com>
2022-09-24 17:04:08 +0300

    correct output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,7 +10,7 @@ def fetch(url):
     import urllib.request as req
     with req.urlopen(url) as response:
         html = response.read()
-        print('Body response: ')
+        print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
         print('\t- utf8 content: {}'.format(html.decode("utf-8")))
