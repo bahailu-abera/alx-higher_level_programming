345d3addff79050bdc7103edb402bfc5a506853d
bahailu <aberabahailu4@gmail.com>
2022-09-24 16:36:33 +0300

    fetch

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module to fetch data from https://alx-intranet.hbtn.io/status
+"""
+
+
+def fetch(url):
+    """ fetch from a URL='url'
+    """
+    import urllib.request as req
+    with req.urlopen(url) as response:
+        html = responde.read()
+        print('Body response: ')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t - utf8 content: {}'.format(html.decode("utf-8")))
+
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    fetch(url)
