3976bfa002871b353acee6d08f6cf69f005b0c70
bahailu <aberabahailu4@gmail.com>
2022-09-24 13:51:09 +0300

    origin

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to make the server respond with "You got me!"
-curl -sL 0.0.0.0:5000/cath_me -X PUT -H "You got me!"
+curl -sL 0.0.0.0:5000/cath_me -X PUT -H "Origin: You got me!"
