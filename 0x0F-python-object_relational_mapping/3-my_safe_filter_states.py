aedd2e3c532e31a52624c8064d2c02c5fb64ff96
bahailu <aberabahailu4@gmail.com>
2023-01-19 22:16:42 +0300

    escape user input

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module filter by user input
+"""
+import MySQLdb
+
+
+def filter_by_user_input(username, password, database, name):
+    """
+    Script that takes in an argument and displays all values
+    in the states table of database where name matches the argument
+    """
+    db = MySQLdb.connect(host='localhost', user=username, passwd=password,
+                         db=database)
+    cur = db.cursor()
+    name = name.split(' ;')
+    sf_name = MySQLdb.escape_string(state_name).decode()
+    sqlquery = ("""SELECT * FROM states WHERE \
+    BINARY states.name='{}'""".format(sf_name))
+    cur.execute(sqlquery)
+    rows = cur.fetchall()
+    cur.close()
+
+    return rows
+
+
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+
+    rows = filter_by_user_input(argv[1], argv[2], argv[3], argv[4])
+    for row in rows:
+        print(row)
