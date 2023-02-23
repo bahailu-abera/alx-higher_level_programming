f795e01d1fc1cbf214785a15a94870fea9c5f1bc
bahailu <aberabahailu4@gmail.com>
2023-02-23 11:36:04 +0300

    fetch all states

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module to list all 'States'
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    """
+    Main method
+    """
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".
+                           format(sys.argv[1],
+                                  sys.argv[2],
+                                  sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session instance
+    with Session() as session:
+
+        # query all the State objects and print them in ascending order by id
+        for state in session.query(State).order_by(State.id).all():
+            print("{}: {}".format(state.id, state.name))
+
+
+if __name__ == "__main__":
+    main()
