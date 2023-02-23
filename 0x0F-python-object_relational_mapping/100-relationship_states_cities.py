73c4776160ea6d31924f5159f99eb3f6a03661ec
bahailu <aberabahailu4@gmail.com>
2023-02-23 16:44:32 +0300

    Create state with cities

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Module creating state and cities and reletes them
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from relationship_state import Base, State
+from relationship_city import City
+
+
+def main():
+    """
+    Main method
+    """
+    state_name = sys.argv[4]
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".
+                           format(sys.argv[1],
+                                  sys.argv[2],
+                                  sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    # Createa a session instance
+    with Session() as session:
+        # Create California state
+        california = State(name="California")
+        session.add(california)
+
+        # Create San Francisco city
+        san_francisco = City(name="San Francisco")
+        california.cities.append(san_francisco)
+        session.commit()
+
+
+if __name__ == "__main__":
+    main()
