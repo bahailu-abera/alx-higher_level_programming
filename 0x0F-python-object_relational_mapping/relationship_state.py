1850c181624ccb2c3b85149e25dffc142993f646
bahailu <aberabahailu4@gmail.com>
2023-02-23 16:35:37 +0300

    state r/n ship

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+First state Model
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+# Define the base class
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State model for states table
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade"all, delete")
